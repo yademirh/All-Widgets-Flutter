7bbf873001acaf43df1221522dde26bc749ef144
Yademir Hernandez <yhernandezs@ufpso.edu.co>
2024-05-07 19:09:01 -0500

    cambio pc

--- lib/presentation/screens/infinity/infinity_screen.dart
+++ lib/presentation/screens/infinity/infinity_screen.dart
@@ -6,15 +6,16 @@ class InfinityScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Infiniyt"),
-      ),
+      // appBar: AppBar(
+      //   title: const Text("Infiniyt"),
+      // ),
       body: const _InfinityView(),
     );
   }
 }
 
 class _InfinityView extends StatefulWidget {
+   
   const _InfinityView();
 
   @override
@@ -23,10 +24,31 @@ class _InfinityView extends StatefulWidget {
 
 class _InfinityViewState extends State<_InfinityView> {
 
+  List<int> imagesId = [10,20,30,40,50];
+
   final ScrollController scroll = ScrollController();
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MediaQuery.removePadding(
+      context: context,
+      removeTop: true,
+      removeBottom: true,
+      child: (ListView.builder(
+          itemCount: imagesId.length,
+          itemBuilder: (context, index) {
+            return FadeInImage(
+              fit: BoxFit.cover,
+              height: 500,
+              width: 300,
+              placeholder: const AssetImage("goku.jpg"),
+              image: NetworkImage(
+                "https://picsum.photos/id/${imagesId[index]}/200/300"
+              )
+            );
+          },
+        ),
+      );
+    ); 
   }
 }
